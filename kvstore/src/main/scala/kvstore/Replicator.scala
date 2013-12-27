7bbf4e6cb0b2aa840d3220a5777d067495d6dd4f
Rahul Revo <rahulrevo@gmail.com>
2013-12-27 01:48:45 -0800

    Implemented replicator to mediate Replication requests with retry and no batching. Tests Step1_PrimarySpec, Step2_SecondarySpec and Step3_ReplicatorSpec pass.

--- kvstore/src/main/scala/kvstore/Replicator.scala
+++ kvstore/src/main/scala/kvstore/Replicator.scala
@@ -11,7 +11,9 @@ import scala.concurrent.duration._
 object Replicator {
   case class Replicate(key: String, valueOption: Option[String], id: Long)
   case class Replicated(key: String, id: Long)
-  
+
+  case class ReplicateTimeOut(seq: Long, r: Replicate)
+
   case class Snapshot(key: String, valueOption: Option[String], seq: Long)
   case class SnapshotAck(key: String, seq: Long)
 
@@ -22,22 +24,50 @@ class Replicator(val replica: ActorRef) extends Actor {
   import Replicator._
   import Replica._
   import context.dispatcher
-  
+  import scala.language.postfixOps
+
   // map from sequence number to pair of sender and request
   var acks = Map.empty[Long, (ActorRef, Replicate)]
-  // a sequence of not-yet-sent snapshots (you can disregard this if not implementing batching)
-  var pending = Vector.empty[Snapshot]
   
-  var _seqCounter = 0L
+  // TODO Implement batching
+  // a sequence of not-yet-sent snapshots for batching
+  var pending = Vector.empty[Snapshot]
+
+  var seq = 0L
   def nextSeq = {
-    val ret = _seqCounter
-    _seqCounter += 1
+    val ret = seq
+    seq += 1
     ret
   }
-  
-  /* TODO Behavior for the Replicator. */
+
+  /* Behavior for the Replicator. */
   def receive: Receive = {
-    case _ =>
+    case r: Replicate => {
+      acks += seq -> (sender, r)
+      replica ! Snapshot(r.key, r.valueOption, seq)
+
+      // Start timer
+      context.system.scheduler.scheduleOnce(100 millisecond, self, ReplicateTimeOut(seq, r))
+
+      nextSeq
+    }
+    case sa: SnapshotAck => {
+      acks.get(sa.seq) match {
+        case Some(value) => {
+          acks -= sa.seq
+          value._1 ! Replicated(value._2.key, value._2.id)
+        }
+        case None => // Ignore since message has already been acknowledged
+      }
+    }
+    case rt: ReplicateTimeOut => {
+      if (acks.contains(rt.seq)) {
+        replica ! Snapshot(rt.r.key, rt.r.valueOption, rt.seq)
+
+        // Start timer
+        context.system.scheduler.scheduleOnce(100 millisecond, self, ReplicateTimeOut(rt.seq, rt.r))
+      }
+    }
   }
 
 }
